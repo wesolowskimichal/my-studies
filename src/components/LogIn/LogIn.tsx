e4edab08b33c4f4af6f37252e3c33d163c51e507
Michal Wesolowski <michal.wesolov@gmail.com>
2024-03-20 14:14:47 +0100

    created login and register pages

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,9 +1,48 @@
+import React, { useState, FormEvent } from 'react'
 import styles from './LogIn.module.scss'
+import { Link } from 'react-router-dom'
 
 function LogIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('logging in: ' + email + ' ' + password)
+  }
+
   return (
-    <div key="login-page-main" className={styles.Wrapper}>
-      <h1>Login Page</h1>
+    <div key="login-page-vertical-wrapper" className={styles.VerticalWrapper}>
+      <div key="login-page-header-wrapper" className={styles.HeaderWrapper}>
+        <h1>Login</h1>
+        <div key="login-page-wrapper" className={styles.Wrapper}>
+          <div key="login-register-wrapper" className={styles.LoginRegisterWrapper}>
+            <button className={`${styles.defaultButton} ${styles.selectedButton}`}>Login</button>
+            <Link to="/register" className={styles.defaultButton}>
+              Register
+            </Link>
+          </div>
+          <form onSubmit={handleLogin} key="login-page-form">
+            <input
+              type="email"
+              name="email-input"
+              id="email-input"
+              placeholder="Email Address"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              name="password-input"
+              id="password-input"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input type="submit" value="Login" />
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,63 @@
+import React, { useState, FormEvent } from 'react'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
+
 function Register() {
-  return <h1>Register Page</h1>
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const [fname, sname] = name.trim().split(' ')
+    console.log(fname + ' ' + sname)
+    if (fname === undefined || sname === undefined) {
+      console.log('ERROR')
+    }
+  }
+
+  return (
+    <div key="login-page-vertical-wrapper" className={styles.VerticalWrapper}>
+      <div key="login-page-header-wrapper" className={styles.HeaderWrapper}>
+        <h1>Register</h1>
+        <div key="login-page-wrapper" className={styles.Wrapper}>
+          <div key="login-register-wrapper" className={styles.LoginRegisterWrapper}>
+            <Link to="/login" className={styles.defaultButton}>
+              Login
+            </Link>
+            <button className={`${styles.defaultButton} ${styles.selectedButton}`}>Register</button>
+          </div>
+          <form onSubmit={handleLogin} key="login-page-form">
+            <input
+              type="email"
+              name="email-input"
+              id="email-input"
+              placeholder="Email Address"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <input
+              type="text"
+              name="name-input"
+              id="name-input"
+              placeholder="Name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <input
+              type="password"
+              name="password-input"
+              id="password-input"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input type="submit" value="Register" />
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Register
