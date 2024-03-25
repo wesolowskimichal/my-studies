15de3eb5cc6f485de39b4995d29d07f7a53429c9
Michal Wesolowski <michal.wesolov@gmail.com>
2024-03-25 12:40:34 +0100

    created api interfaces

---
+++ src/components/interfaces.tsx
@@ -0,0 +1,61 @@
+export interface AccountType {
+  id: Number
+  type: string
+}
+
+export interface User {
+  id: Number
+  first_name: string
+  last_name: string
+  user_type: AccountType
+  picture: ImageBitmap
+}
+
+export interface Repository {
+  id: Number
+  name: string
+  picture: ImageBitmap
+  owners: User[]
+}
+
+export interface RepositoryEnrolment {
+  id: Number
+  user: User
+  repository: Repository
+  status: boolean
+}
+
+export interface RepositoryPost {
+  id: Number
+  owner: User
+  repository: Repository
+  title: string
+  description: string
+  attachment: File
+  isTask: boolean
+  pinned: boolean
+  due_to: Date
+  created_at: Date
+}
+
+export interface RepositoryTaskResponse {
+  id: Number
+  owner: User
+  task: RepositoryPost
+  attachment: File
+  uploaded_at: Date
+  mark: Number
+}
+
+export interface RepositoryTaskResponseComment {
+  owner: User
+  repository_task_response: RepositoryTaskResponse
+  comment: string
+}
+
+export interface Notification {
+  owner: User
+  is_read: boolean
+  message: string
+  sent_at: Date
+}
