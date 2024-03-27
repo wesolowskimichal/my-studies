12cba18efaae9a18659ebbb6236b51376529d513
Michal Wesolowski <michal.wesolov@gmail.com>
2024-03-27 12:46:03 +0100

    implemented course filtering

--- src/components/AllCourses/AllCourses.tsx
+++ src/components/AllCourses/AllCourses.tsx
@@ -19,6 +19,8 @@ const apiResponse = async () => {
 
 function AllCourses() {
   const [courses, setCourses] = useState<Repository[]>([])
+  const [visibleCourses, setVisibleCourses] = useState<Repository[]>([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     const getAllCourses = async () => {
@@ -26,6 +28,7 @@ function AllCourses() {
         const repositories = await apiResponse()
         console.log(repositories)
         setCourses(repositories)
+        setVisibleCourses(courses)
       } catch (error) {
         console.error('Error fetching user: ', error)
       }
@@ -35,11 +38,30 @@ function AllCourses() {
     console.log(courses)
   }, [])
 
+  useEffect(() => {
+    if (search.length === 0) {
+      setVisibleCourses(courses)
+    } else {
+      const filteredCourses = courses.filter(course => course.name.toLowerCase().includes(search.toLowerCase()))
+      setVisibleCourses(filteredCourses)
+    }
+  }, [search, courses])
+
   return (
-    <>
-      <div className={styles.Wrapper}>
+    <div className={styles.Wrapper}>
+      <div className={styles.FindBox}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Szukaj kursu"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+      </div>
+      <div className={styles.Courses}>
         {courses.length > 0 ? (
-          courses.map((course, index) => (
+          visibleCourses.map((course, index) => (
             <div key={index}>
               <Course key={index} repository={course} />
             </div>
@@ -48,7 +70,7 @@ function AllCourses() {
           <p>Loading...</p>
         )}
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/MainPage/Header/Header.tsx
+++ src/components/MainPage/Header/Header.tsx
@@ -2,8 +2,9 @@ import styles from './Header.module.scss'
 import logo from '../../../assets/logo.png'
 import LoggedPanel from './LoggedPanel/LoggedPanel'
 import { Link } from 'react-router-dom'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { User } from '../../interfaces'
+import { getUserFromApi } from '../../api/functions'
 
 interface HeaderProps {
   currentElement: string
@@ -11,9 +12,18 @@ interface HeaderProps {
   user: User | undefined
 }
 
-function Header({ currentElement, setCurrentElement, user }: HeaderProps) {
-  console.log(user)
-  const isLogged = user !== undefined
+function Header({ currentElement, setCurrentElement }: HeaderProps) {
+  const [isLogged, setIsLogged] = useState(false)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const apiUser = await getUserFromApi()
+        setIsLogged(true)
+      } catch (error) {}
+    }
+    fetchUser()
+  }, [])
   return (
     <div className={styles.Wrapper}>
       <div className={styles.ContentWrapper}>
